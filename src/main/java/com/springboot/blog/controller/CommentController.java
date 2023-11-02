b9a1e9b9dc2714ee96d38ca96e9cf38472baa8f6
kienpham2121 <kienpt375@gmail.com>
2023-11-02 23:04:02 +0700

    [KienPT] commit source api get all comment

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -1,8 +1,11 @@
 package com.springboot.blog.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +26,10 @@ public class CommentController {
 	public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") int postId,
 			@RequestBody CommentDto commentDto) {
 		return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
-
+	}
+	@GetMapping("/posts/{postId}/comment")
+	public ResponseEntity<List<CommentDto>> getListCommentByPostId(@PathVariable(value = "postId") int postId){
+		return new ResponseEntity<>(commentService.getCommentsByPostId(postId), HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/springboot/blog/repository/CommentRepository.java
+++ src/main/java/com/springboot/blog/repository/CommentRepository.java
@@ -1,5 +1,7 @@
 package com.springboot.blog.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.springboot.blog.entity.Comment;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>{
-	
+	List<Comment> findByPostId(long postId);
 }
